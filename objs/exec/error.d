e70cf899b11a453abc0c62bdd9051537b4472ebf
Paula FERREIRA <pferreir@e2r3p6.clusters.42paris.fr>
2023-09-02 23:44:20 +0200

    main changed

---
+++ objs/exec/error.d
@@ -0,0 +1,8 @@
+objs/exec/error.o: srcs/exec/error.c includes/minishell.h \
+  includes/../libft/libft.h libft/get_next_line.h
+
+includes/minishell.h:
+
+includes/../libft/libft.h:
+
+libft/get_next_line.h:
---
+++ objs/exec/exec.d
@@ -0,0 +1,8 @@
+objs/exec/exec.o: srcs/exec/exec.c includes/minishell.h \
+  includes/../libft/libft.h libft/get_next_line.h
+
+includes/minishell.h:
+
+includes/../libft/libft.h:
+
+libft/get_next_line.h:
