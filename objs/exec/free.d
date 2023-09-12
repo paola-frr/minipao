90018cb6b252d222f878c6642aefac01c6c278f6
Daria SYDELNYKOVA <dsydelny@made-f0Cr11s2.clusters.42paris.fr>
2023-09-12 21:56:14 +0200

    unquote

--- objs/exec/free.d
+++
@@ -1,8 +0,0 @@
-objs/exec/free.o: srcs/exec/free.c includes/minishell.h \
-  includes/../libft/libft.h libft/get_next_line.h
-
-includes/minishell.h:
-
-includes/../libft/libft.h:
-
-libft/get_next_line.h:
